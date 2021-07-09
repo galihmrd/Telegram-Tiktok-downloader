42353c40078633335f6b2deb54b6572f54b57392
Imszy17 <galihmardiana15@gmail.com>
2021-07-09 22:30:09 +0700

    initial push

--- bot/handlers/messages.py
+++ bot/handlers/messages.py
@@ -11,7 +11,14 @@ async def get_message(message: Message):
     for api in platforms:
         if videos := [v for v in await api.handle_message(message) if v and v.content]:
             for video in videos:
+                lol = await message.reply("Mengunduh...")
                 await bot.send_video(
                     message.chat.id, video.content, reply_to_message_id=message.message_id
                 )
+                await lol.delete()
             break
+
+@dp.message_handler(commands=['start'])
+async def start(message: Message):
+    await message.reply("Untuk mengunduh video, cukup kirimkan tautan video tiktok anda")
+
