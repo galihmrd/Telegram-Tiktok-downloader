8bbad9d1eed20889015fd504c86e26f87972c133
Imszy17 <galihmardiana15@gmail.com>
2021-07-09 22:53:58 +0700

    Ttdown: test fix start command

--- bot/handlers/messages.py
+++ bot/handlers/messages.py
@@ -1,4 +1,4 @@
-from aiogram.types import Message
+from aiogram import types
 from bot import dp, bot
 from bot.api import MobileTikTokAPI, TikTokAPI
 
@@ -7,18 +7,19 @@ platforms = [MobileTikTokAPI(), TikTokAPI()]
 
 
 @dp.message_handler()
-async def get_message(message: Message):
+async def get_message(message: types.Message):
+    lol = await message.reply("Mengunduh video...")
     for api in platforms:
         if videos := [v for v in await api.handle_message(message) if v and v.content]:
             for video in videos:
-                lol = await message.reply("Mengunduh...")
+                lol = await message.reply("Mengunggah video...")
                 await bot.send_video(
                     message.chat.id, video.content, reply_to_message_id=message.message_id
                 )
                 await lol.delete()
             break
 
-@dp.message_handler(commands=['start'])
-async def start(message: Message):
+@dp.message_handler(commands=['start', 'help'], commands_prefix='!/')
+async def start(message: types.Message):
     await message.reply("Untuk mengunduh video, cukup kirimkan tautan video tiktok anda")
 
